f57a86ebdfbf7b5a688603d91dcf00b487fc19a9
Viraj Rane <110802415+VirajRane7@users.noreply.github.com>
2023-09-27 22:55:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,45 +11,52 @@ let b3 = 0;
 let b4 = 0;
 
  function fun() {
-    setInterval(async function () {
+   //  setInterval(async function () {
+   // await axios.get("https://blynk.cloud/external/api/get?token=WmcxdFqb008EAVLUyMXGbtiRPSZiSzUv&V1")
+   //  .then((res)=>{
+   //      console.log(res.data);
+   //  }).catch((err)=>{
+   //      console.log("Error");
+   //  })
+   //  fetch("https://blynk.cloud/external/api/get?token=WmcxdFqb008EAVLUyMXGbtiRPSZiSzUv&V1")
+   //  .then((response)=>{
+   //      return response.json()
+   //  }).then((value)=>{
+   //      // console.log(value)
+   //      // if (value == 1) {
+   //      //     if (state1==0) {
+   //      //         state1 = 1;
+   //      //         checkState(state1, "Button_1");
+   //      //         isStop1 = false;
+   //      //         timer1()
+   //      //     }
+            
+   //      // //    console.log(b1); 
+            
+   //      // }else{  
+   //      //     if (state1==1) {
+                
+   //      //         state1=0;
+   //      //         checkState2(state1, "Button_1");
+   //      //         isStop1 = true;
+   //      //     }
+   //      // //    console.log("Button 1 off"); 
+            
+
+   //      // }
+   //      console.log(value)
+   //  })
+   //      console.log(b1);
+   //      b1++;
+   //      b3++
+   //  },1000)
+
    await axios.get("https://blynk.cloud/external/api/get?token=WmcxdFqb008EAVLUyMXGbtiRPSZiSzUv&V1")
     .then((res)=>{
-        console.log(res.data);
+        console.log("hey"+res.data+ "hey" );
     }).catch((err)=>{
         console.log("Error");
     })
-    fetch("https://blynk.cloud/external/api/get?token=WmcxdFqb008EAVLUyMXGbtiRPSZiSzUv&V1")
-    .then((response)=>{
-        return response.json()
-    }).then((value)=>{
-        // console.log(value)
-        // if (value == 1) {
-        //     if (state1==0) {
-        //         state1 = 1;
-        //         checkState(state1, "Button_1");
-        //         isStop1 = false;
-        //         timer1()
-        //     }
-            
-        // //    console.log(b1); 
-            
-        // }else{  
-        //     if (state1==1) {
-                
-        //         state1=0;
-        //         checkState2(state1, "Button_1");
-        //         isStop1 = true;
-        //     }
-        // //    console.log("Button 1 off"); 
-            
-
-        // }
-        console.log(value)
-    })
-        console.log(b1);
-        b1++;
-        b3++
-    },1000)
 }
 
 
