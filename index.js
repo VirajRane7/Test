f9e7695e1d0bda219fda2d086ca0bb019974822e
Viraj Rane <110802415+VirajRane7@users.noreply.github.com>
2023-09-28 12:54:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ let b2 = 0;
 let b3 = 0;
 let b4 = 0;
 
-async function fun() {
-   //  setInterval(async function () {
+ function fun() {
+    setInterval(async function () {
    // await axios.get("https://blynk.cloud/external/api/get?token=WmcxdFqb008EAVLUyMXGbtiRPSZiSzUv&V1")
    //  .then((res)=>{
    //      console.log(res.data);
@@ -49,42 +49,50 @@ async function fun() {
    //      console.log(b1);
    //      b1++;
    //      b3++
-   //  },1000)
 
-   // await axios.get("https://blynk.cloud/external/api/get?token=WmcxdFqb008EAVLUyMXGbtiRPSZiSzUv&V1")
-   //  .then((res)=>{
-   //      console.log("hey"+res.data+ "hey" );
-   //  }).catch((err)=>{
-   //      console.log("Error");
-   //  })
-     await fetch("https://jsonplaceholder.typicode.com/todos/1")
+       fetch("https://jsonplaceholder.typicode.com/todos/1")
     .then((response)=>{
         return response.json()
     }).then((value)=>{
-        // console.log(value)
-        // if (value == 1) {
-        //     if (state1==0) {
-        //         state1 = 1;
-        //         checkState(state1, "Button_1");
-        //         isStop1 = false;
-        //         timer1()
-        //     }
-            
-        // //    console.log(b1); 
-            
-        // }else{  
-        //     if (state1==1) {
-                
-        //         state1=0;
-        //         checkState2(state1, "Button_1");
-        //         isStop1 = true;
-        //     }
-        // //    console.log("Button 1 off"); 
-            
-
-        // }
+        
         console.log(value)
     })
+    },1000)
+
+   // // await axios.get("https://blynk.cloud/external/api/get?token=WmcxdFqb008EAVLUyMXGbtiRPSZiSzUv&V1")
+   // //  .then((res)=>{
+   // //      console.log("hey"+res.data+ "hey" );
+   // //  }).catch((err)=>{
+   // //      console.log("Error");
+   // //  })
+   //   await fetch("https://jsonplaceholder.typicode.com/todos/1")
+   //  .then((response)=>{
+   //      return response.json()
+   //  }).then((value)=>{
+   //      // console.log(value)
+   //      // if (value == 1) {
+   //      //     if (state1==0) {
+   //      //         state1 = 1;
+   //      //         checkState(state1, "Button_1");
+   //      //         isStop1 = false;
+   //      //         timer1()
+   //      //     }
+            
+   //      // //    console.log(b1); 
+            
+   //      // }else{  
+   //      //     if (state1==1) {
+                
+   //      //         state1=0;
+   //      //         checkState2(state1, "Button_1");
+   //      //         isStop1 = true;
+   //      //     }
+   //      // //    console.log("Button 1 off"); 
+            
+
+   //      // }
+   //      console.log(value)
+   //  })
 }
 
 
